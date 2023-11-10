f4bc2a96ac3d8d751f2ed12af91edde4e0e1a4f7
rabigorkhali <rabigorkhali@macbookairs-MacBook-Pro.local>
2023-11-10 19:57:48 +0545

    chore: routes path modified for laravel ui  and design made simple for homepage,navbar

--- application-files/resources/views/system/layouts/navbar.blade.php
+++ application-files/resources/views/system/layouts/navbar.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
-        <a class="navbar-brand" href="{{ url('/') }}">
+        <a class="navbar-brand" href="{{ url('/system/home') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
 
--- application-files/routes/web.php
+++ application-files/routes/web.php
@@ -17,10 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
      return view('welcome');
 });
-
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::group([ 'prefix' => 'system', 'middleware' => ['auth']], function () {
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::resource('users', UserController::class);
 });
 
